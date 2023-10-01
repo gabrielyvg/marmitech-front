9150eed77be8d75b24a4da0c47ed71f4bb9e701f
gabrielyvg <gabrielyvg@outlook.com>
2023-10-01 17:10:17 -0300

    feature: primeiro commit - esqueleto do projeto, tela de login com primeiras configurações para ser feita a autenticação de usuário, com redirecionamento para a tela home

---
+++ marmitech-front/src/pages/index.js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import styles from "@/styles/Login.module.css";
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/router";
+import { authService } from "../services/authService";
+
+export default function Home() {
+  const titulo = "Flor de sal";
+  const subtitulo = "Marmitech";
+  const router = useRouter();
+
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      };
+    })
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    if (values.username == '' || values.password == '') {
+      alert("Necessário preencher ambos os campos!");
+    }
+
+    authService
+      .login({
+        username: values.username,
+        password: values.password,
+      })
+      .then(() => {
+        router.push('/home');
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Usuário ou a senha estão inválidos')
+      })
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Marmitech</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <h1>{titulo}</h1>
+          <h4>{subtitulo}</h4>
+          <div className={styles.containerElements}>
+            <form onSubmit={onSubmit}>
+              <input onChange={handleChange} type="text"
+                name="username" values={values.username}
+                placeholder="Usuário" />
+              <input onChange={handleChange} type="password"
+                name="password" value={values.password}
+                placeholder="Senha" />
+              <button type="submit">Entrar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
